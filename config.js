8908f0996ac90aa894f6fd775eb1ffd794974ad6
khattab88 <m_khatab_88@hotmail.com>
2021-01-12 16:11:29 +0200

    update expresso services package

--- config.js
+++ config.js
@@ -4,8 +4,9 @@ module.exports = {
     port: process.env.PORT || 3000,
     dbConnection: process.env.DATABASE_CONNECTION,
     email: {
-        host: process.env.EMAIL,
         address: process.env.EMAIL_ADDRESS,
-        password: process.env.EMAIL_PASSWORD,
+        host: process.env.EMAIL_HOST,
+        userName: process.env.EMAIL_USERNAME,
+        password: process.env.EMAIL_PASSWORD
     }
 };
